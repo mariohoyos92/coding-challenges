c8b3db869f8fc44ad9d9086761a66cc88ecaac5e
Mario Hoyos <mariohoyos92@gmail.com>
2018-04-13 10:14:36 -0500

    chore: complete challenge

--- codewars.js
+++ codewars.js
@@ -850,3 +850,39 @@ function cleanItUp(str) {
   }
   return splitArr.map(word => word.toUpperCase()[0] + word.slice(1)).join(" ");
 }
+
+// Born a misinterpretation of this kata, your task here is pretty simple: given an array of values and an amount of beggars, you are supposed to return an array with the sum of what each beggar brings home, assuming they all take regular turns, from the first to the last.
+
+// For example: [1,2,3,4,5] for 2 beggars will return a result of [9,6], as the first one takes [1,3,5], the second collects [2,4].
+
+// The same array with 3 beggars would have in turn have produced a better out come for the second beggar: [5,7,3], as they will respectively take [1,4], [2,5] and [3].
+
+// Also note that not all beggars have to take the same amount of "offers", meaning that the length of the array is not necessarily a multiple of n; length can be even shorter, in which case the last beggers will of course take nothing (0).
+
+function beggars(values, n) {
+  if (n === 0) {
+    return [];
+  }
+  const resultsArr = [];
+  let currentBeggar = 0;
+
+  values.forEach(amount => {
+    resultsArr[currentBeggar] = resultsArr[currentBeggar]
+      ? resultsArr[currentBeggar] + amount
+      : amount;
+    currentBeggar++;
+    if (currentBeggar === n) {
+      currentBeggar = 0;
+    }
+  });
+
+  if (resultsArr.length < n) {
+    let leftoutBeggars = n - resultsArr.length;
+    while (leftoutBeggars > 0) {
+      resultsArr.push(0);
+      leftoutBeggars--;
+    }
+  }
+
+  return resultsArr;
+}
