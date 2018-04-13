059f11c661ac68048d70ed0382a9875365735aad
Mario Hoyos <mariohoyos92@gmail.com>
2018-04-13 09:57:20 -0500

    chore: complete challenge

--- codewars.js
+++ codewars.js
@@ -815,3 +815,38 @@ function frogJump(arr) {
   }
   return jumpCount;
 }
+
+// We've begun receiving transmissions from Planet Gibber again after many decades of silence. Unfortunately, this time they've been all garbled up!
+
+// Luckily, linguists were able to figure out the rules of Gibberish the last time we heard from them. They've given us a guide on how to identify the actual bits of language from the random data in the strings we receive.
+
+// In order to parse the Gibberish from the... well, gibberish, we'll need to follow these rules:
+
+// Gibberish, as we read it, only contains letters: remove all non-letter characters (including spaces)
+// Gibberish is only made up of 5 letter words: separate this into words that are all 5 letters long - no trailing words with fewer than 5 letters!
+// Gibberish word are all capitalised: so capitalise all the words (sort of like German nouns)
+// Some of the transmissions are only garbled data. If you are given an empty string or a string that doesn't contain any letters, then please return an empty string at the end.
+
+// Even if you can't read the Gibberish, that's okay as long as you've formatted it according to the language rules!
+
+// Good luck :)
+
+function cleanItUp(str) {
+  let filteredArr = str
+    .toLowerCase()
+    .split("")
+    .filter(char => true)
+    .filter(char => char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122);
+
+  let splitArr = [];
+  let tempWord = "";
+
+  for (let i = 0; i < filteredArr.length; i++) {
+    tempWord += filteredArr[i];
+    if ((i + 1) % 5 === 0) {
+      splitArr.push(tempWord);
+      tempWord = "";
+    }
+  }
+  return splitArr.map(word => word.toUpperCase()[0] + word.slice(1)).join(" ");
+}
