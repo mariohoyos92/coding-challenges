e94eec1890dcd945b66fed1e4a780085be8b7af8
Mario Hoyos <mariohoyos92@gmail.com>
2018-04-27 13:34:16 -0500

    chore: complete challenge

--- codewars.js
+++ codewars.js
@@ -886,3 +886,15 @@ function beggars(values, n) {
 
   return resultsArr;
 }
+
+// Complete the function that counts the number of unique consonants in a string (made up of printable ascii characters).
+
+// Consonants are letters used in English other than "a", "e", "i", "o", "u". We will count "y" as a consonant.
+
+// Remember, your function needs to return the number of unique consonants - disregarding duplicates. For example, if the string passed into the function reads "add", the function should return 1 rather than 2, since "d" is a duplicate.
+
+// Similarly, the function should also disregard duplicate consonants of differing cases. For example, "Dad" passed into the function should return 1 as "d" and "D" are duplicates.
+
+function countConsnants(str) {
+  return new Set(str.toLowerCase().replace(/[^a-z]|[aeiou]/g, "")).size;
+}
